31c0d5f194a6db160e983015da634c5b6d7b5229
PhyuPhyu <phyuphyuthinweb.it@gmail.com>
2026-03-12 23:25:53 +0400

    Add loops and practise exercises

---
+++ src/NumberTriangleThree.java
@@ -0,0 +1,13 @@
+public class NumberTriangleThree {
+    public static void main(String[] args) {
+        int k = 1;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(3 * k);
+                System.out.print("\t");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
